2d46247f3c2073f6d9bf95491090c90af7305900
hongru <hongru.chenhr@alibaba-inc.com>
2013-07-07 16:46:32 +0800

    update

--- django_app/jeeart/static_media/js/pages/home.js
+++ django_app/jeeart/static_media/js/pages/home.js
@@ -4,6 +4,9 @@
 	function _getDom () {
 		dom.$ctrls = $('.shortcut-ctrl li');
 		dom.$ctrlPages = $('#shortcut-list ul');
+        dom.$list = $('#shortcut-list');
+        dom.$listVP = dom.$list.find('.sl-viewport');
+        dom.$listCW = dom.$list.find('.sl-con-wrapper');
 	}
 
 	function _bind () { 
@@ -13,14 +16,27 @@
 			//console.log(_ind)
 			dom.$ctrlPages.removeClass('current');
 			dom.$ctrlPages.eq(_ind).addClass('current');
+            
+            var l = -(_ind*dom.$listVP.width());
+            dom.$listCW.animate({
+                'left': l
+            });
 
 			dom.$ctrls.removeClass('current');
 			$(this).parents('li').addClass('current');
 		});
 	}
+    
+    function _setupList () {
+        var uln = dom.$list.find('.sl-con-wrapper>ul').length,
+            sw = dom.$listVP.width();
+            
+        dom.$listCW.width(uln*sw);
+    }
 
 	function __init () {
 		_getDom();
+        _setupList();
 		_bind();
 
 		//init gallery
