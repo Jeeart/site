46a0d74b212b1f86cff99191b3afdd0850e03021
root <root@AY130715124946283210Z.(none)>
2013-07-15 21:59:20 +0800

    update to aliyun

---
+++ django_app/jeeart/wsgi.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import django.core.handlers.wsgi
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'jeeart.settings_server'
+#app_apth = "/home/horizonchen/jeeart/site/django_app/jeeart/"
+app_apth = "/root/Work/jeeart/site/django_app/jeeart/"
+
+sys.path.append(app_apth)
+application = django.core.handlers.wsgi.WSGIHandler()
